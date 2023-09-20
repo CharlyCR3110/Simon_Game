31be54fdfe9cad64e1ec1b1a065d7df54ac2c0f8
charly <carlosgarita3110@gmail.com>
2023-09-20 11:18:03 -0600

    feat(model): add ModelView interface

---
+++ src/main/java/com/simon/ejemploBase/model/ModelView.java
@@ -0,0 +1,10 @@
+package com.simon.ejemploBase.model;
+
+public interface ModelView {
+
+	// La interfaz de la Vista-Modelo provee los métodos de acceso
+	// al estado del modelo que son permitidos a la vista.
+	// Normalmente, son métodos utilizados para actualizar la
+	// interfaz, y no permiten completar modificaciones.
+	//
+}
