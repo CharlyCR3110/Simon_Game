34ff23f600aedea23ca7958ce5ff5d2999fe8ce3
charly <carlosgarita3110@gmail.com>
2023-09-21 21:09:10 -0600

    refactor(model): enhance Model class
    The Model class has been enhanced with the following improvements:
    
    - Added fields for managing the game state, including `sequence`, `currentRound`, `maxColors`, and `gameOver`.
    - Modified the constructor to accept a maximum number of colors (`maxColors`) as a parameter.
    - Added a `startNewGame` method to initialize a new game.
    - Implemented a `generateNextColor` method to generate the next color in the sequence.
    - Added `getNextSequence`, `getSequence`, `getCurrentRound`, and `getMaxColors` methods to access game data.
    - Introduced a `setGameOver` method to update the game state.
    - Implemented a `nextRound` method to advance to the next round.
    
    These changes make the Model class more robust and suitable for managing the Simon Says game logic.

--- src/main/java/com/simon/ejemploBase/model/Model.java
+++ src/main/java/com/simon/ejemploBase/model/Model.java
@@ -2,11 +2,63 @@ package com.simon.ejemploBase.model;
 
 import com.simon.mvc.ObservableModel;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Random;
 
 public class Model extends ObservableModel implements ModelView {
+	private Queue<Integer> sequence;    // Secuencia de colores
+	private int currentRound; // Número de ronda actual
+	private int maxColors; // Número máximo de colores
+	private boolean gameOver; // Indica si el juego ha terminado
 
 	public Model() {
 		System.out.println("Inicializando modelo..");
+	}
+
+	public Model(int maxColors) {
+		this.maxColors = maxColors; // Corregir el nombre del parámetro aquí
+		this.sequence = new LinkedList<>();
+		this.currentRound = 0;
+		this.gameOver = false;
+	}
+
+	public void startNewGame() {
+		if (this.sequence == null) {
+			this.sequence = new LinkedList<>();
+		}
+		this.sequence.clear();
+		this.currentRound = 0;
+		this.gameOver = false;
+		generateNextColor();
+		updateData("startNewGame");
+		System.out.println("SYSO startNewGame");
+	}
+
+	public void generateNextColor() {
+		Random random = new Random();
+		int nextColor = random.nextInt(maxColors);
+		sequence.add(nextColor);
+	}
+
+	public Queue<Integer> getNextSequence() {
+		nextRound();
+		return sequence;
+	}
+	public Queue<Integer> getSequence() {
+		return sequence;
+	}
+
+	public int getCurrentRound() {
+		return currentRound;
+	}
+
+	public int getMaxColors() {
+		return maxColors;
+	}
+
+	public boolean isGameOver() {
+		return gameOver;
 	}
 
 	private void updateData(String msg) {
@@ -22,4 +74,13 @@ public class Model extends ObservableModel implements ModelView {
 		notifyListeners(String.format("%s", msg), this);
 	}
 
+	public void setGameOver(boolean gameOver) {
+		this.gameOver = gameOver;
+	}
+
+	public void nextRound() {
+		currentRound++;
+		generateNextColor();
+		updateData("nextRound");
+	}
 }
