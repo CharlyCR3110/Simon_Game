20c821780dd9562b22709222a610f9cf4a84013b
charly <carlosgarita3110@gmail.com>
2023-09-23 16:03:15 -0600

    feat(xml): add XmlPersister class for game configuration

---
+++ src/main/java/com/simon/xml/XmlPersister.java
@@ -0,0 +1,37 @@
+package com.simon.xml;
+
+import com.simon.ejemploBase.configuration.GameConfig;
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.Marshaller;
+import jakarta.xml.bind.Unmarshaller;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+public class XmlPersister {
+	private String path;
+	private static XmlPersister theInstance;
+	public static XmlPersister instance(){
+		if (theInstance==null) theInstance=new XmlPersister("src/main/resources/game/config/gameConfig.xml");
+		return theInstance;
+	}
+	public XmlPersister(String p) {
+		path=p;
+	}
+	public GameConfig load() throws Exception{
+		JAXBContext jaxbContext = JAXBContext.newInstance(GameConfig.class);
+		FileInputStream is = new FileInputStream(path);
+		Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+		GameConfig result = (GameConfig) unmarshaller.unmarshal(is);
+		is.close();
+		return result;
+	}
+	public void store(GameConfig gameConfig)throws Exception{
+		JAXBContext jaxbContext = JAXBContext.newInstance(GameConfig.class);
+		FileOutputStream os = new FileOutputStream(path);
+		Marshaller marshaller = jaxbContext.createMarshaller();
+		marshaller.marshal(gameConfig, os);
+		os.flush();
+		os.close();
+	}
+}
